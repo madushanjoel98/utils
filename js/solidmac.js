2117e730b4168d372b13f9f00d112463c73b17fa
Madushan joel <42777460+madushanjoel98@users.noreply.github.com>
2024-12-17 09:55:17 +0530

    add dark mood

--- js/solidmac.js
+++ js/solidmac.js
@@ -1,53 +1,29 @@
-import './jquery';
-var BRLISTER = "brnu";
-
-
-console.log("HELLO");
-var i= await SessionStroageRunnerJSON;
-
-function salterExample() {
-
-    var f = [
-
-        { "name": "joel" },
-        { "name": "madushan" },
-    ];
+var darkmode = "darkmode";
 
 
 
-
-
-
-    return f;
-
-}
-
-async function SessionStroageRunnerJSON(data, key, url, method) {
-    try {
-      const response = await fetch(url, {
-        method: method,
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
-  
-      const responseData = await response.json();
-  
-      // Get the current data in sessionStorage
-      let op = sessionStorage.getItem(key);
-      
-      // If sessionStorage is empty or the lengths of the data are different, update sessionStorage
-      if (!op || op.length !== JSON.stringify(responseData).length) {
-        sessionStorage.setItem(key, JSON.stringify(responseData));
-      } else {
-        return sessionStorage.getItem(key);
-      }
-  
-      console.log(responseData);
-  
-    } catch (error) {
-      console.log(error);
+function OnchangeDarkmood() {
+    if ($('#night').prop('checked')) {
+        localStorage.setItem(darkmode,1);
+        $( "body" ).addClass( "bg-dark text-white" );
+        console.log('Checkbox is checked');
+    } else {
+        localStorage.setItem(darkmode,0);
+        console.log('Checkbox is not checked');
+        $( "body" ).removeClass( "bg-dark text-white" );
     }
   }
-  
+
+
+function checkDarkMood() {
+if(  localStorage.getItem(darkmode) == 1 ){
+    $('#night').prop('checked', true);
+    $( "body" ).addClass( "bg-dark text-white" );
+}else{
+    
+  $( "body" ).removeClass( "bg-dark text-white" );
+   
+    $('#night').prop('checked', false);
+}
+
+  }
